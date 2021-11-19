629865645b62f1eeefe2fbb170c31b788b96b785
Gustavo G Herreira <gustavogherreira@gmail.com>
2021-11-19 10:30:30 -0300

    Adjusted Application.py to PEP 8 standard

--- Application.py
+++ Application.py
@@ -11,38 +11,39 @@ if __name__ == '__main__':
         print('---' * 15)
 
         try:
-            decision = int(input('O que deseja fazer:\n1- Cadastrar um novo usuário.\n2- Entrar em minha conta.\n'
-                             '3- finalizar o programa.\n[user]: '))
+            decision = int(input('O que deseja fazer:\n'
+                                 '1- Cadastrar um novo usuário.\n2- Entrar em minha conta.\n'
+                                 '3- finalizar o programa.\n[user]: '))
         except ValueError:
             print("Por favor, digite apenas números.")
             continue
 
         if decision not in (1, 2, 3):
             print('input invalido!')
-        else:
-            if decision == 1:
-                print("provavelmente uma janela para cadastro se abriu em segundo plano")
-                registerApp = RegisterRoot()
-                while not registerApp.finished:
-                    registerApp.update()
-                registerApp.destroy()
-                continue
+            continue
 
-            elif decision == 2:
-                print("provavelmente uma janela para autenticação se abriu em segundo plano")
-                AuthenticatorApp = Authenticator()
-                result = AuthenticatorApp.identify_user()
-                if result:
-                    userdata = db.get_user_by_name(result)
-                    user = User(userdata[0], userdata[1], userdata[2], userdata[3], userdata[4], userdata[5])
-                    print(user.show_values())
+        if decision == 1:
+            print("provavelmente uma janela para cadastro se abriu em segundo plano")
+            registerApp = RegisterRoot()
+            while not registerApp.finished:
+                registerApp.update()
+            registerApp.destroy()
+            continue
 
-                    AuthenticatedUser = AuthenticatedRoot(user)
-                    while not AuthenticatedUser.finished:
-                        AuthenticatedUser.update()
-                    AuthenticatedUser.destroy()
-                continue
+        elif decision == 2:
+            print("provavelmente uma janela para autenticação se abriu em segundo plano")
+            AuthenticatorApp = Authenticator()
+            result = AuthenticatorApp.identify_user()
+            if result:
+                userdata = db.get_user_by_name(result)
+                user = User(userdata[0], userdata[1], userdata[2], userdata[3], userdata[4], userdata[5])
 
-            elif decision == 3:
-                print('finalizando programa...')
-                exit()
+                AuthenticatedUser = AuthenticatedRoot(user)
+                while not AuthenticatedUser.finished:
+                    AuthenticatedUser.update()
+                AuthenticatedUser.destroy()
+            continue
+
+        elif decision == 3:
+            print('finalizando programa...')
+            exit()
